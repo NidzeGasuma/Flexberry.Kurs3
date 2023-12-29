e4d573100f7ee868ae3246bc586678a9c35e7711
Flexberry-man <mail@flexberry.net>
2023-12-29 16:06:31 +0000

    Flexberry code generation

---
+++ src/ember-app/app/enums/i-i-s-kurs3-тип-атр.js
@@ -0,0 +1,10 @@
+import { createEnum } from 'ember-flexberry-data/utils/enum-functions';
+
+export default createEnum({
+  string: '',
+  int: '',
+  float: '',
+  DateTime: '',
+  bool: '',
+  double: ''
+});
